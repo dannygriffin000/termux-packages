bf0089b9180334bd0c00a65719025cf213dae217
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-20 17:46:08 +0300

    bump revisions of changed packages

--- packages/util-linux/build.sh
+++ packages/util-linux/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://en.wikipedia.org/wiki/Util-linux
 TERMUX_PKG_DESCRIPTION="Miscellaneous system utilities"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.33.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.kernel.org/pub/linux/utils/util-linux/v${TERMUX_PKG_VERSION:0:4}/util-linux-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=631be8eac6cf6230ba478de211941d526808dba3cd436380793334496013ce97
 TERMUX_PKG_DEPENDS="ncurses, libcrypt, zlib"
